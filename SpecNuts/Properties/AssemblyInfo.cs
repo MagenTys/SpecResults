71eb215da300ed69b0cf0e407b95638343d637d9
0x4D616E75 <0x4d616e75@elektronische-nachricht.de>
2019-01-20 10:08:09 +0100

    Moving assembly informations from AssemblyInfo.cs to Specuts.csproj

--- SpecNuts/Properties/AssemblyInfo.cs
+++ SpecNuts/Properties/AssemblyInfo.cs
@@ -1,15 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SpecNuts")]
-[assembly: AssemblyDescription("A forked from SpecResults project to provide the ability to generate test result in cucumber format.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("William Sia")]
-[assembly: AssemblyProduct("SpecNuts")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("301e04c6-bc1b-4da4-83f0-5fdf76fea8bb")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
