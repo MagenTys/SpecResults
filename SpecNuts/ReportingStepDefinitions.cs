7f4507da827b1629241b956e7c0976c55f129e84
0x4D616E75 <0x4d616e75@elektronische-nachricht.de>
2019-01-20 09:59:37 +0100

    Removing System.Runtime.Remoting dependencies, these aren't supported by .netstandard2.0

--- SpecNuts/ReportingStepDefinitions.cs
+++ SpecNuts/ReportingStepDefinitions.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
-using SpecNuts.ReportingAspect;
 
 namespace SpecNuts
 {
-	[Reporting]
 	public abstract class ReportingStepDefinitions : ContextBoundObject
 	{
 		public async Task ReportStep(Func<Task> stepFunc, params object[] args)
