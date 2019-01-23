e9b0d8ed47a3f22d4d291509b357662afa577078
0x4D616E75 <0x4d616e75@elektronische-nachricht.de>
2019-01-23 14:42:12 +0100

    move SpecNuts.ApprovalTestSuite to new csproj format and set target framework to .netcoreapp2.1

--- SpecNuts.ApprovalTestSuite/Properties/AssemblyInfo.cs
+++ SpecNuts.ApprovalTestSuite/Properties/AssemblyInfo.cs
@@ -5,14 +5,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("SpecNuts.ApprovalTestSuite")]
-[assembly:
-	AssemblyDescription(
-        "A forked from SpecResults.ApprovalTestSuite project to provide the ability to generate json file in cucumber format.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("William Sia")]
-[assembly: AssemblyProduct("SpecNuts.ApprovalTestSuite")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -25,17 +17,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("a5289c34-47cc-4acf-80ef-d286a5779959")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
